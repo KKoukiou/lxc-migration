b06df55ba09096f9592d1254cb49a64d3fe457a7
root <root@xenon7.cslab.ece.ntua.gr>
2016-03-10 13:00:41 +0200

    script for the pre-dump and dump phase including the resync phase if required

---
+++ dump.sh
@@ -0,0 +1,102 @@
+#!/bin/bash 
+
+CNAME=$1
+ITERS=$2
+HOST=$3
+PID=$4
+rsync_pid=$5
+PORT=12345
+echo "CANEM = $CNAME ITERS = $ITERS HOST = $HOST pid = $PID "
+
+PAUSE=0
+
+
+checkpointdir=/var/lib/lxc/$CNAME/checkpoint
+
+general_args="--tcp-established \
+--file-locks \
+--link-remap \
+--force-irmap \
+--manage-cgroups \
+--ext-mount-map auto \
+--enable-external-sharing \
+--enable-external-masters \
+--enable-fs hugetlbfs \
+--enable-fs tracefs \
+-vvvvvv \
+-t $PID"
+#--freeze-cgroup /sys/fs/cgroup/freezer/lxc/$CNAME"
+
+mkdir $checkpointdir	
+ssh $HOST "mkdir $checkpointdir"
+pages_written=10000000
+fin=0
+for iter in $(seq 1 $ITERS); do 
+	echo "Iteration $iter"
+	sleep $PAUSE
+	mkdir "$checkpointdir/$iter/"
+	if [ $iter -eq 1 ] ; then
+		# First snapshot -- no parent, kill afterwards
+		if [ $ITERS -eq 1 ]; then 
+			action="dump"
+			args="-s --track-mem"
+		else
+			action="pre-dump"
+			args="--leave-running --track-mem"
+		fi
+	elif [ $iter -eq $ITERS ] || [ $pages_written -lt 1024 ]; then 
+		# Last snapshot -- has parent, kill afterwards
+		#args="--prev-images-dir=$checkpointdir/$((iter - 1))/ --track-mem --leave-stopped"
+		args="--prev-images-dir=../$((iter - 1))/ --track-mem --leave-stopped"
+		action="dump"
+		fin=1
+	else
+		# Other snapshots -- have parent, keep running
+		#args="--prev-images-dir=$checkpointdir/$((iter - 1))/ --track-mem --leave-running"
+		args="--prev-images-dir=../$((iter - 1))/ --track-mem --leave-running"
+		action="pre-dump"
+	fi
+	echo "criu $action -D $checkpointdir/$iter -o $checkpointdir/$iter/$action.log $general_args $args "
+	
+	criu $action -D $checkpointdir/$iter -o $checkpointdir/$iter/$action.log $general_args $args 
+	if grep -q "finished successfully" $checkpointdir/$iter/$action.log 
+	then
+		grep "finished successfully" $checkpointdir/$iter/$action.log  
+	else	
+		grep "Error" $checkpointdir/$iter/$action.log 
+		echo "Seems to have failed"
+		return 0 
+	fi
+ 
+	ssh $HOST "mkdir $checkpointdir/$iter"		
+
+	#Kill the rsync process and continue with a new one. That will be the last rsync	
+	sync_start=$(date -u +"%s%6N")
+	if [ $fin -eq 1 ] ; then
+		kill -9 $rsync_pid
+		rsync -aAXHltzh --progress --numeric-ids --devices --rsync-path="sudo rsync" $checkpointdir/$iter/ $HOST:/$checkpointdir/$iter/
+		sync_finish=$(date -u +"%s%6N")	
+		diff=$(($sync_finish- $sync_start))
+		echo "Final Rsync: $diff μs\n" |tee -a statistic.txt
+	fi
+	
+	du -sh $checkpointdir/$iter/ | tee -a statistic.txt
+	
+	if [ $fin -eq 1 ] && [ $ITERS -eq 1 ]; then 
+		echo "Final dump completed"
+		break 
+	else
+		echo "$action: Iteration $iter "2>$1 | tee -a statistic.txt
+		(cd criu && ./crit decode -i $checkpointdir/$iter/stats-dump --pretty) 2>$1 | tee -a statistic.txt
+		pages_written="$(cd criu && ./crit decode -i $checkpointdir/$iter/stats-dump --pretty |& grep "pages_written" | awk '{print $2}' | head -c -2 )"
+		echo "pages_written = $pages_written"
+		if [ $fin -eq 1 ]; then 
+			echo "Final dump completed"
+			break 
+		fi
+	fi
+done
+
+return $iter
+
+
